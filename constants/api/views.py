bd3a9bc9049f254bb381c63be78ff51e0d76f047
parthsujalshah <parthsujalshah@gmail.com>
2020-08-12 23:28:01 +0530

    permission

--- constants/api/views.py
+++ constants/api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from ..models import *
+from rest_framework.permissions import IsAuthenticated
 
 class GetAllConstantsView(RetrieveAPIView):
+
+  permission_classes = [IsAuthenticated]
+
   def retrieve(self, request, *args, **kwargs):
     response = NumOfTweetsPerState.objects.all()[0].value * 36 + NumOfTweetsPerDay.objects.all()[0].value * DaysOfTrendingTweets.objects.all()[0].value + NoOfPopularTweets.objects.all()[0].value
     return Response(response)
